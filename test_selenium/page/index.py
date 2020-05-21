4ed9f25518659067d7bc28ac1bda181f0bcbae21
Jason <45475367@qq.com>
2020-05-21 19:13:06 +0800

    代码重构，加入BasePage

--- test_selenium/page/index.py
+++ test_selenium/page/index.py
@@ -1,11 +1,19 @@
 from selenium.webdriver.common.by import By
 
+from test_selenium.page.base_page import BasePage
 from test_selenium.page.register import Register
 
 
-class Index:
-    def __init__(self, driver):
-        self.driver = driver
+class Index(BasePage):
+    _base_url = "https://work.weixin.qq.com/"
+
+    # def __init__(self, driver=None):
+    #     if driver is None:
+    #         self.driver = webdriver.Chrome()
+    #         self.driver.implicitly_wait(3)
+    #         self.driver.get("https://work.weixin.qq.com/")
+    #     else:
+    #         self.driver = driver
 
     def goto_register(self):
         self.driver.find_element(By.LINK_TEXT, "立即注册").click()
